01f1e7594698a272118fb13924bc70671238ce94
Piyulgaur <piyulgaurkvongc@gmail.com>
2022-09-20 02:23:23 +0530

    sep20 changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS =[
     BASE_DIR,"static"
 ]
+
